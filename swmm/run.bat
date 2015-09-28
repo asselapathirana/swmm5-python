493cdac560cc1ab0fd70f5ddc6e90af25b76c3fc
Assela Pathirana <assela@pathirana.net>
2015-09-28 11:38:58 +0700

    python 5.1.0.10 source added-again

--- swmm/run.bat
+++ swmm/run.bat
@@ -1,23 +1,38 @@
 :: echo off
-set arch=64bit
-:::name and version variables are used by setup.py. They are very important. 
-set name=SWMM5
-set version=1.0.0.1dev
+:: 
+:: first step both in swmm5.c and swmm5.h #undef WINDOWS after its definition
+:: then make sure mkstemp is not used to create temporary files in 
+:: char* getTempFileName(char* fname) function (desable WINDOWS macro use
+:: Run this script for each python version x architecture combination
+:: then manually upload swmm version to to pypi
+
+:: MAKE SURE to edit version and name in setup.py too!! (Fix this later so that run.bat does not have to specify it!!)
+
+:: rst2html.bat file needs to be copied to the pythonxxx/scripts directory of winpython. New versions seem not to come with this file! Anyway to fix this by having rst2html.bat version in the source?
+
+:: github repo user is asselapathirana 
+
+:: create a release in github and upload relevant EPA-SWMM version. 
 
 set good="false"
 if not "%1" == "" (
 	set name=%1
 	if not "%2"=="" (
 		set arch=%2
-		if not  "%3"=="" (
-			set version=%3
-			set good="true"
+		if not "%3"=="" (
+			set pyversion=%3
+			if not  "%4"=="" (
+				set version=%4
+				set good="true"
+			) 
 		) 
-	) 
+	)
 )
 
+echo "Hi", %good%
+
 if not %good%=="true" (
-	echo "USAGE: %0% <name (case is important check on pypi!)> <arch 64bit/32bit> <version x.x.x.x[dev]>
+	echo "USAGE: %0% <name (case is important check on pypi!)> <arch 64bit/32bit> <python_version x.x.x.x> <version x.x.x.x[dev]>
 	goto ERROR
 )
 
@@ -27,26 +42,6 @@ set loc=%~dp0
 set py3=3.3.5.0
 set py2=2.7.6.4
 
-if defined sdkdir (
-	set sdkv=%SdkDir:~40,4%
-	)
-if defined sdksetupdir (
-	set sdkv=%SdkSetupDir:~40,4%
-	)
-
-if defined sdkdir (
-	if defined sdksetupdir (
-		echo "Problem: environment is mixed between SDK 7.0 and 7.1"
-		goto ERROR
-	)
-)
-
-if %sdkv%==v7.0 (
-	set pyversion=%py2%
-)
-if %sdkv%==v7.1 (
-	set pyversion=%py3%
-)
 
 ::2.7.6.4 
 :: 3.3.5.0
@@ -55,19 +50,6 @@ set pv=%pyver:~0,1%
 set pyverwhl=%pyver:.=%
 
 
-
-if %pv%==2 (
-	set env="C:\Program Files\Microsoft SDKs\Windows\v7.0\Bin\SetEnv.cmd"
-	)
-if %pv%==3 (
-    set env="C:\Program Files\Microsoft SDKs\Windows\v7.1\Bin\SetEnv.cmd"
-	)
-
-if %pv%=="" (
-	echo "Unknown python version" %pv%
-	goto ERROR
-	)
-	
 	
 if %arch% == 64bit (
 	set arch1=x64
@@ -89,10 +71,9 @@ echo Python version (Wheel)     : %pyverwhl%
 echo Python family (2,3)        : %pv%
 echo Architecture               : %arch% %arch1% %arch2% %arch3%
 echo Environment for compiler   : %env%
-echo SDK version                : %sdkv%
 pause 
-set DISTUTILS_USE_SDK=1 
-call %env% /%arch1% /release   || goto ERROR
+:: set DISTUTILS_USE_SDK=1 
+:: call %env% /%arch1% /release   || goto ERROR
 echo on
 
 cd /d %loc%
@@ -125,9 +106,12 @@ python tests\test_1.py -v || goto ERROR
 pip install nose 
 python tests\test_multithreading.py || goto ERROR
 set PATH=%OLDPATH%
+
+call env1\Scripts\deactivate.bat || goto ERROR
+del env1 /f /q
 :: test wheel
 
-del env1 /f /q
+
 virtualenv --no-site-packages --clear env1 || goto ERROR
 echo on
 call %loc%\env1\Scripts\activate.bat || goto ERROR
