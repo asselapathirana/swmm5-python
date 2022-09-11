d06ee66c27ad6585428a2e2dda7e5de871a00bac
Ubuntu <ubuntu@vps-0f0d20b0.vps.ovh.net>
2022-09-11 13:19:03 +0000

    Revert "change setup for linux"
    This reverts commit 384911eeae674d651e3ddbffc7c31c3db827cf0d.

--- swmm/setup.py
+++ swmm/setup.py
@@ -30,41 +30,34 @@ else:
     linkerargs   = ['-fopenmp','-Wno-deprecated','-O3','-Wno-error']
 
     class custom_build_ext(build_ext):
-        def run(self):
-            for ext in self.extensions:
-                self.build_cmake(ext)
-            super().run()
+        cwd = pathlib.Path().absolute()
+        # these dirs will be created in build_py, so if you don't have
+        # any python sources to bundle, the dirs will be missing
+        build_temp = pathlib.Path(self.build_temp)
+        build_temp.mkdir(parents=True, exist_ok=True)
+        extdir = pathlib.Path(self.get_ext_fullpath(ext.name))
+        extdir.mkdir(parents=True, exist_ok=True)
+        # example of cmake args
+        config = 'Debug' if self.debug else 'Release'
+        cmake_args = [
+            '-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=' + str(extdir.parent.absolute()),
+            '-DCMAKE_BUILD_TYPE=' + config
+        ]
 
-        def build_cmake(self, ext):
-            cwd = pathlib.Path().absolute()
+        # example of build args
+        build_args = [
+            '--config', config,
+            '--', '-j4'
+        ]
 
-            # these dirs will be created in build_py, so if you don't have
-            # any python sources to bundle, the dirs will be missing
-            build_temp = pathlib.Path(self.build_temp)
-            build_temp.mkdir(parents=True, exist_ok=True)
-            extdir = pathlib.Path(self.get_ext_fullpath(ext.name))
-            extdir.mkdir(parents=True, exist_ok=True)
+        os.chdir(str(build_temp))
+        self.spawn(['cmake', str(cwd)] + cmake_args)
+        if not self.dry_run:
+            self.spawn(['cmake', '--build', '.'] + build_args)
+        # Troubleshooting: if fail on line above then delete all possible 
+        # temporary CMake files including "CMakeCache.txt" in top level dir.
+        os.chdir(str(cwd))
 
-            # example of cmake args
-            config = 'Debug' if self.debug else 'Release'
-            cmake_args = [
-                '-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=' + str(extdir.parent.absolute()),
-                '-DCMAKE_BUILD_TYPE=' + config
-            ]
-
-            # example of build args
-            build_args = [
-                '--config', config,
-                '--', '-j4'
-            ]
-
-            os.chdir(str(build_temp))
-            self.spawn(['cmake', str(cwd)] + cmake_args)
-            if not self.dry_run:
-                self.spawn(['cmake', '--build', '.'] + build_args)
-            # Troubleshooting: if fail on line above then delete all possible 
-            # temporary CMake files including "CMakeCache.txt" in top level dir.
-            os.chdir(str(cwd))
 
 with open("README.txt","r") as f:
     README=f.read()
