20a3c3b69b5f5076ad865d3e15dbb238b1a2e531
Assela Pathiranaj <assela@pathirana.net>
2022-09-11 20:14:17 +0000

    added setup2.py for testing cmake with scikit-build

---
+++ swmm/setup2.py
@@ -0,0 +1,13 @@
+from skbuild import setup
+
+setup(
+    name="SWMM5",
+    version="5.2.1",
+    description="a minimal example package (cpp version)",
+    author='Assela Pathirana',
+    license="MIT",
+    packages=['swmm5'],
+    python_requires=">=3.7",
+
+
+)
